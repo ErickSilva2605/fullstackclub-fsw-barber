ac1c1ed41cbaf64b67e4c673c0cdbb15a8a5529f
ErickSilva2605 <erick.dev.it@gmail.com>
2024-02-03 12:45:07 -0300

    feat: redirecionando usuario não atutenticado para home

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/app/_components/ui/button";
+
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
 import { Service } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -13,6 +13,8 @@ interface BarbershopDetailsPageProps {
 }
 
 const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
+
   if (!params.id) {
     // TODO: redirecionar para home page
     return null;
@@ -38,7 +40,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
 
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.services.map((service: Service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
         ))}
       </div>
     </div>
